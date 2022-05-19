9c655748cf9a72405f622b20ae9e550399194de8
ddrobecq <didier@drobecq.fr>
2022-05-19 12:06:37 +0200

    Sanitize strings in SQL statements

--- src/users/usersdb.js
+++ src/users/usersdb.js
@@ -8,40 +8,40 @@ function getOne (id, body){
 
 /* GET ALL USERS */
 function getAll (id, body){
-    let strreq = 'SELECT * FROM user';
+    let strreq = "SELECT * FROM user";
     return (db.execSQL(strreq));
 };
 
 /* UPDATE A USER */
 const update = function (id, body) {
-    let strreq = 'UPDATE user \
-    SET usr_name = "' + db.escape (body.usr_name) + '" WHERE usr_id=' + id;
+    let strreq = "UPDATE user \
+    SET usr_name = " + db.escape (body.usr_name) + " WHERE usr_id=" + id;
     console.log (strreq);
     return (db.execSQL(strreq));
 };
 
 /* INSERT A USER */
 const create = function (id, body) {
-    let strreq = 'INSERT INTO user (usr_name) VALUES ("' + db.escape (body.usr_name)  +  '")';
+    let strreq = "INSERT INTO user (usr_name) VALUES (" + db.escape (body.usr_name)  +  ")";
     return (db.execSQL(strreq));
 };
 
 /* GET ALL games FOR A GIVEN user */
 const getallGames = function (id, body) {
-    let strreq = 'SELECT * FROM game WHERE (usr_id=' + id + ')';
+    let strreq = "SELECT * FROM game WHERE (usr_id=" + id + ")";
     return (db.execSQL(strreq));
 };
 
 /* GET AVG FOR A GIVEN user */
 const getStats = function (id, body) {
-    let strreq = 'SELECT (sum(gam_pocket)/sum(gam_shot)) as avgPocket, \
+    let strreq = "SELECT (sum(gam_pocket)/sum(gam_shot)) as avgPocket, \
     (sum(gam_foul)/sum(gam_shot)) as avgFoul, \
     min(gam_foul/gam_shot) as minFoul, \
     max(gam_foul/gam_shot) as maxFoul , \
     min(gam_pocket/gam_shot) as minPocket, \
     max(gam_pocket/gam_shot) as maxPocket \
-    FROM game WHERE (usr_id=' + id + ') \
-    GROUP BY usr_id';
+    FROM game WHERE (usr_id=" + id + ") \
+    GROUP BY usr_id";
     return (db.execSQL(strreq));
 };
 
