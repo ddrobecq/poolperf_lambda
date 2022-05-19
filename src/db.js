4e94878313cf85f1f9b690adb5a284b900dea195
ddrobecq <didier@drobecq.fr>
2022-05-19 21:53:01 +0200

    Go back to single Connection

--- src/db.js
+++ src/db.js
@@ -1,12 +1,5 @@
 const mysql = require('mysql');
 
-var pool = mysql.createPool({
-    host: process.env.DBHOST,
-    user: process.env.DBUSER,
-    password: process.env.DBPWD,
-    database: process.env.DBNAME
-});
-
 /* EXECUTE SQL QUERY WITH A PROMISE */
 exports.escape = function (str) {
     return mysql.escape (str);
@@ -14,15 +7,21 @@ exports.escape = function (str) {
 
 /* EXECUTE SQL QUERY WITH A PROMISE */
 exports.execSQL = function (strreq) {
+    const con = mysql.createConnection({
+        host: process.env.DBHOST,
+        user: process.env.DBUSER,
+        password: process.env.DBPWD,
+        database: process.env.DBNAME
+    });
+
     let p = new Promise (function (res, rej) {
-        pool.getConnection(err, con) {
-            con.query(strreq, function (err, result) {
-                if (err) rej (err); 
-                else{
-                    res (result); 
-                }
-            });            
-        };
+        con.query(strreq, function (err, result) {
+            if (err) rej (err); 
+            else{
+                res (result); 
+            }
+        });  
+        con.end();      
     });
     return p;
 };
