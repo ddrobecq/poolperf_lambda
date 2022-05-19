373909570947eb090067228f093328e0b0dbf1ee
ddrobecq <didier@drobecq.fr>
2022-05-19 11:37:46 +0200

    Bug fixed isNan instead of isNan

--- src/games/gamesdb.js
+++ src/games/gamesdb.js
@@ -5,7 +5,7 @@ const create = function (id, body) {
     if (!isNaN (body.player.playerId))
         if (!isNaN (body.player.nbShot)) 
             if (!isNaN (body.player.nbPocket)) 
-                if (!isNan (body.player.nbShot)) {
+                if (!isNaN (body.player.nbShot)) {
                     let strreq = 'INSERT INTO game (usr_id, gam_shot, gam_pocket, gam_foul) \
                     VALUES (' + body.player.playerId + ',' + body.player.nbShot + ',' + body.player.nbPocket + ',' + body.player.nbFoul +  ')';
                     
